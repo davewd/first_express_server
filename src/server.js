22c04c4a3f48e866a448a25c126a4b9160878066
davewd <davewd@me.com>
2019-02-01 00:14:32 +1100

    Add a few extra helpful packages

--- src/server.js
+++ src/server.js
@@ -1,5 +1,10 @@
 const express = require('express')
+var morgan  = require('morgan')
+const debug = require('debug')
+const argv = require('yargs').argv
+
 const app = express()
+app.use(morgan('combined'))
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
